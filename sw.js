8589237e3e60a1dfe0caf179b70146295a771233
Austin Verburg <dairyboycomics@gmail.com>
2018-06-10 12:33:05 -0700

    fetch cache

--- sw.js
+++ sw.js
@@ -22,3 +22,39 @@ self.addEventListener("install", event => {
         })
     );
 });
+
+self.addEventListener("fetch", event => {
+    let cacheRequest = event.request;
+    let cacheUrlObj = new URL(event.request.url);
+    if (event.request.url.indexOf("restaurant.html") > -1) {
+        const cacheURL = "restaurant.html";
+        cacheRequest = new Request(cacheURL);
+    }
+    if (cacheUrlObj.hostname !== "localhost") {
+        event.request.mode = "no-cors";
+    }
+cache
+    event.respondWith(
+        caches.match(cacheRequest).then(response => {
+            return (
+                response ||
+                fetch(event.request)
+                    .then(fetchResponse => {
+                        return caches.open(cacheID).then(cache => {
+                            cache.put(event.request.fetchResponse.clone());
+                            return fetchResponse;
+                        });
+                    })
+                    .catch(error => {
+                        if (event.request.url.indexOf(".jpg") > -1) {
+                            return caches.match("/img/na.png");
+                        }
+                        return new Response("Application is not connected to the internet", {
+                            status: 404,
+                            statusText: "Application is not connected to the internet"
+                        });
+                    })
+            );
+        })
+    );
+})
