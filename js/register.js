5de45db88a8b1144224b781bf26180a7bb04a336
Austin Verburg <dairyboycomics@gmail.com>
2018-06-11 22:23:38 -0700

    service worker tells app to wait

--- js/register.js
+++ js/register.js
@@ -1,9 +1,9 @@
 /* Registering the service worker */
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js").then(reg => { 
-        console.log("Service Worker Registered!");
-    }).catch(error => {
-        console.log("Registration failed :( ");
+    navigator.serviceWorker.register("/sw.js").then( function(reg) { 
+        console.log("Service Worker Registered! :D");
+    }).catch( function(error) {
+        console.log("Registration failed :(");
     })
 }
---
+++ sw.js
@@ -0,0 +1,9 @@
+/* Create static cache */
+var staticCache = "static-cache-v1";
+
+/* Installing the service worker */
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        
+    );
+})
