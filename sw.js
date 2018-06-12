cd5555beed64354f39a86db96c9a22129c9096a9
Austin Verburg <dairyboycomics@gmail.com>
2018-06-11 23:08:56 -0700

    changed port for submission

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 var staticCache = "static-cache-v1";
 
 /* Installing the service worker */
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
     event.waitUntil( //App is instructed to wait 
         caches.open(staticCache).then(function(cache) {
             return cache.addAll([ //adds items into new static-cache-v1
@@ -23,19 +23,19 @@ self.addEventListener('install', function(event) {
     );
 });
 
-/* Listen for fetch event, return cache items if it has them */
+/* Listen for fetch event */
 self.addEventListener("fetch", function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return ( response || fetch(event.request).then(function(fetchResponse) {
-                return caches.open(staticCache).then(function(cache) {
+                return caches.open(staticCache).then(function(cache) { //returns cached items if it has them
                         cache.put(event.request, fetchResponse.clone());
                         return fetchResponse;
                     });
                 }).catch(function(error) {
-                    return new Response("Not connected to internet", {
+                    return new Response("Not connected to internet ._.", {
                         status: 404,
-                        statusText: "Not connected to internet"
+                        statusText: "Not connected to internet ._."
                     });
                 })
             );
