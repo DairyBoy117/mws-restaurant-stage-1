f60369df08c5567211c8d0e79e2e2a03c867eb55
Austin Verburg <dairyboycomics@gmail.com>
2018-06-11 22:30:06 -0700

    add items to cache

--- sw.js
+++ sw.js
@@ -3,7 +3,20 @@ var staticCache = "static-cache-v1";
 
 /* Installing the service worker */
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        
+    event.waitUntil( //App is instructed to wait 
+        caches.open(staticCache).then(function(cache) {
+            return cache.addAll([ //adds items into new static-cache-v1
+                "/",
+                "/index.html",
+                "/css/styles.css",
+                "/css/responsive.css",
+                "/data/restaurants.json",
+                "/js/",
+                "/js/dbhelper.js",
+                "/js/main.js",
+                "/js/restaurant_info.js",
+                "/js.register.js"
+            ])
+        })
     );
-})
+});
