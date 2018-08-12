73003786b512f085cd0ca47f3f7e5bff0d034a5c
Austin Verburg <dairyboycomics@gmail.com>
2018-08-12 15:44:59 -0700

    Reverted

---
+++ sw.js
@@ -0,0 +1,43 @@
+var staticCache = "static-cache-v1";
+
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(staticCache).then(function(cache) {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/css/styles.css",
+                "/css/responsive.css",
+                "/data/restaurants.json",
+                "/js/",
+                "/js/dbhelper.js",
+                "/js/main.js",
+                "/js/restaurant_info.js",
+                "/js.register.js"
+            ]).catch(function(error){
+                console.log("Failed to open cache D:")
+            });
+        })
+    );
+});
+
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return ( response || fetch(event.request).then(function(fetchResponse) {
+                return caches.open(staticCache).then(function(cache) {
+                        cache.put(event.request, fetchResponse.clone());
+                        return fetchResponse;
+                    });
+                }).catch(function(error) {
+                    return new Response("Not connected to internet ._.", {
+                        status: 404,
+                        statusText: "Not connected to internet ._."
+                    });
+                })
+            );
+        })
+    );
+});
